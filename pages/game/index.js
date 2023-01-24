a3bae81a365ca1525837c333ee6cfac9e6149f2a
jaxonhill <jaxoncharleshill@gmail.com>
2023-01-24 11:26:35 -0800

    Add switch statement based on game state

--- pages/game/index.js
+++ pages/game/index.js
@@ -6,7 +6,8 @@ export default function GameScreen() {
     const [firstPost, setFirstPost] = useState(null);
     const [secondPost, setSecondPost] = useState(null);
     const [correctAnswerID, setCorrectAnswerID] = useState(null);
-    const [userAnswerID, setUserAnswerID] = useState(null);
+    const [userScore, setUserScore] = useState(0);
+    const [gameState, setGameState] = useState("playing");
 
 
     const getAndSetPostsAndRightAnswer = async () => {
@@ -27,29 +28,41 @@ export default function GameScreen() {
     }
 
     function handleVote(chosenPost) {
-        setUserAnswerID(chosenPost.id);
+        if (chosenPost.id === correctAnswerID) {
+            setGameState("roundWin");
+        } else {
+            setGameState("gameOver");
+        }
     }
 
     console.log(firstPost);
     console.log(secondPost);
-    console.log(userAnswerID);
-    console.log(correctAnswerID)
 
     // Error checking: Check that both posts do not have the same upvotes or id. 
-    // If they do, then refetch.
+    // If they do, then refetch. MAKE THIS INTO THE FUNCTION (get_random_posts). SHOULD NOT BE OUT HERE.
     if (firstPost && secondPost) {
         if (firstPost.id === secondPost.id || firstPost.upvotes === secondPost.upvotes) {
             handleRoundStart();
         }
     }
 
-    return (
-        <div className="flex flex-col gap-12">
-            {(userAnswerID && (userAnswerID === correctAnswerID)) && <p>Correct!</p>}
-            <button onClick={handleRoundStart} className="bg-white shadow py-2 rounded-2xl">New Round</button>
-            {firstPost && <PostSection post={firstPost} handleVote={handleVote} />}
-            <p className="text-center text-gray-600 font-bold text-5xl">OR</p>
-            {secondPost && <PostSection post={secondPost} handleVote={handleVote} />}
-        </div>
-    )
+    switch (gameState) {
+        case ("playing"):
+            return (
+                <div className="flex flex-col gap-12">
+                    <button onClick={handleRoundStart} className="bg-white shadow py-2 rounded-2xl">New Round</button>
+                    {firstPost && <PostSection post={firstPost} handleVote={handleVote} />}
+                    <p className="text-center text-gray-600 font-bold text-5xl">OR</p>
+                    {secondPost && <PostSection post={secondPost} handleVote={handleVote} />}
+                </div>
+            )
+        case ("roundWin"):
+            return (
+                <p className="text-center">Correct!</p>
+            )
+        case ("gameOver"):
+            return (
+                <p className="text-center">Game Over!</p>
+            )
+    }
 }
